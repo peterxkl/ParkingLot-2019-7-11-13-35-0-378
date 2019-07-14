af9693b9813c70a98967c556093faa43ebfa3bb2
Dillon <18711345569@163.com>
2019-07-14 15:44:57 +0800

    feat:finish story1/ac2

--- src/test/java/com/thoughtworks/tdd/Story1Test.java
+++ src/test/java/com/thoughtworks/tdd/Story1Test.java
@@ -18,4 +18,22 @@ public class Story1Test {
         //then
         assertSame(car,car1);
     }
+    @Test
+    public void should_return_correspond_car_when_fetch_mutiple_cars(){
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        ParkingBoy boy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+        Car car1 = new Car();
+
+        //when
+        Ticket ticket = boy.parking(car);
+        Ticket ticket1 = boy.parking(car1);
+        Car car_back = boy.fetching(ticket);
+        Car car1_back = boy.fetching(ticket1);
+
+        //then
+        assertSame(car_back,car);
+        assertSame(car1_back,car1);
+    }
 }
